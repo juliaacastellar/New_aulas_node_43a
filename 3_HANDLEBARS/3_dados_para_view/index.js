662b5d8222bce2a118ec5f1b054391b78a3c7316
Julia Castellar <castellar2003j@gmail.com>
2023-11-18 00:05:29 -0300

    enviando dados para view

---
+++ 3_HANDLEBARS/3_dados_para_view/index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const exphbs = require("express-handlebars")
+
+const app = express()
+
+// configurando o handlebars como template engine
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+    const usuario = {
+        nome: "julia",
+        profissao: "estudante",
+        idade: 20
+    }
+
+
+    res.render('home', {usuario})
+})
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000!")
+})
